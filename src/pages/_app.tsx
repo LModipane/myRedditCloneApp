e2119a07fdf8fb68f6c4a9b46a4103fed5fe8c64
shaun <modipanesh@gmail.com>
2023-03-06 12:37:20 +0200

    extend chakra themes to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
+import { theme } from '@/Chakra/theme';
 import { ChakraBaseProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<ChakraBaseProvider>
+    <ChakraBaseProvider theme={ theme}>
 			<Component {...pageProps} />
 		</ChakraBaseProvider>
 	);
