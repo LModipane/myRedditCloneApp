1caf4e1c82b6e80e8a47b890e03271f3387fe424
shaun <modipanesh@gmail.com>
2023-03-06 14:37:32 +0200

    fixed some styling

--- src/components/Layout/NavBar/SearchInput/index.tsx
+++ src/components/Layout/NavBar/SearchInput/index.tsx
@@ -11,18 +11,18 @@ const Searchinput = (props: Props) => {
 			<InputGroup>
 				<InputLeftElement
 					pointerEvents="none"
-					children={<SearchIcon color="gray.300" mb="1.5"/>}
+					children={<SearchIcon color="gray.700" mb="1.5"/>}
 				/>
 				<Input
-                    border="1px solid gray"
+                    // border="1px solid gray"
                     type="text"
                     placeholder="Search Reddit..."
                     fontSize="10pt"
-                    _hover={{ bg: 'white', border: '1px solid', borderColor: 'blue.300' }}
-                    _placeholder={{ color: "gray.500" }}
+                    _hover={{ bg: 'gray.50', border: '1px solid', borderColor: 'blue.300' }}
+                    _placeholder={{ color: "gray.700" }}
                     _focus={{ outline: 'none', border: '1px solid', borderColor: 'blue.500' }}
                     height="34px"
-                    bg="gray.50"
+                    bg="gray.100"
 				/>
 			</InputGroup>
 		</Flex>
