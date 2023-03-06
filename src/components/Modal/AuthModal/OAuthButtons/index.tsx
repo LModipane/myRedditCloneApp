43dee56fb0d2d0817da2ea253df8d3a9e95c88a2
shaun <modipanesh@gmail.com>
2023-03-06 16:55:14 +0200

    added the login and sign components to OAuthButtons

--- src/components/Modal/AuthModal/OAuthButtons/index.tsx
+++ src/components/Modal/AuthModal/OAuthButtons/index.tsx
@@ -1,9 +1,21 @@
+import { authModalState } from '@/atoms/authmodal';
+import { useRecoilValue } from 'recoil';
+import { Flex } from '@chakra-ui/react';
 import React from 'react';
+import { LOGIN_VIEW, SIGNIN_VIEW } from '@/lib/constants/authModalViewStates';
+import LogIn from './LogIn';
+import SignIn from './SignIn';
 
 type Props = {};
 
-const OAuthInputs = (props: Props) => {
-	return <div>OAuthInputs</div>;
+const OAuthButtons = (props: Props) => {
+	const modalState = useRecoilValue(authModalState);
+	return (
+		<Flex>
+			{modalState.view === LOGIN_VIEW && <LogIn />}
+			{modalState.view === SIGNIN_VIEW && <SignIn />}
+		</Flex>
+	);
 };
 
-export default OAuthInputs;
+export default OAuthButtons;
