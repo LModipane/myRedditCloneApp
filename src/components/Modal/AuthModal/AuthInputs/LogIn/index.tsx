c62c4563b2fb9a3f7cf0ddcf9641191bb06c4d34
shaun <modipanesh@gmail.com>
2023-03-06 17:15:18 +0200

    added form to login component

--- src/components/Modal/AuthModal/AuthInputs/LogIn/index.tsx
+++ src/components/Modal/AuthModal/AuthInputs/LogIn/index.tsx
@@ -1,9 +1,16 @@
+import { Button, Input } from '@chakra-ui/react';
 import React from 'react';
 
 type Props = {};
 
 const Login = (props: Props) => {
-	return <div>Login</div>;
+	return (
+		<form>
+			<Input name="email" type="email" placeholder="enter email" />
+			<Input name="password" type="password" placeholder="enter password" />
+			<Button type="submit">Log in</Button>
+		</form>
+	);
 };
 
 export default Login;
