2c6b1a685106861de6f794cbea6fc352e281714c
shaun <modipanesh@gmail.com>
2023-03-06 19:43:26 +0200

    styled sign in component

--- src/components/Modal/AuthModal/AuthInputs/SignIn/index.tsx
+++ src/components/Modal/AuthModal/AuthInputs/SignIn/index.tsx
@@ -1,9 +1,83 @@
+import { Box, Input, Button, Flex, Text } from '@chakra-ui/react';
 import React from 'react';
 
 type Props = {};
 
 const SignIn = (props: Props) => {
-	return <div>SignIn</div>;
+	return (
+		<Box>
+			<form>
+				<Input
+					required
+					name="email"
+					type="email"
+					placeholder="Enter email"
+					_placeholder={{ color: 'gray.700' }}
+					_hover={{
+						bg: 'gray.50',
+						border: '1px solid',
+						borderColor: 'blue.300',
+					}}
+					_focus={{
+						outline: 'none',
+						border: '1px solid',
+						borderColor: 'blue.500',
+					}}
+					bg="gray.100"
+					mb="3"
+				/>
+				<Input
+					required
+					placeholder="Enter password"
+					type="password"
+					name="password"
+					_placeholder={{ color: 'gray.700' }}
+					_hover={{
+						bg: 'gray.50',
+						border: '1px solid',
+						borderColor: 'blue.300',
+					}}
+					_focus={{
+						outline: 'none',
+						border: '1px solid',
+						borderColor: 'blue.500',
+					}}
+					bg="gray.100"
+					mb="3"
+				/>
+				<Input
+					required
+					placeholder="Confirm assword"
+					type="password"
+					name="confirmPassword"
+					_placeholder={{ color: 'gray.700' }}
+					_hover={{
+						bg: 'gray.50',
+						border: '1px solid',
+						borderColor: 'blue.300',
+					}}
+					_focus={{
+						outline: 'none',
+						border: '1px solid',
+						borderColor: 'blue.500',
+					}}
+					bg="gray.100"
+					mb="3"
+				/>
+				<Button width="100%" height="36px" mt="2" mb="2" type="submit">
+					Sign Up
+				</Button>
+			</form>
+			<Flex justify="center" mt="4" fontSize="12pt">
+				<Text mr="2" fontWeight="400" color="black">
+					Already An Redditor?
+				</Text>
+				<Text color="blue.500" fontWeight="700" cursor="pointer">
+					Log In
+				</Text>
+			</Flex>
+		</Box>
+	);
 };
 
 export default SignIn;
