2ca6d8362b5a97b818962d807c22b294c685eb01
shaun <modipanesh@gmail.com>
2023-03-06 14:38:12 +0200

    defined authbuttons and authmodal components

--- src/components/Layout/NavBar/RightContent/index.tsx
+++ src/components/Layout/NavBar/RightContent/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Flex } from '@chakra-ui/react';
+import AuthModal from './AuthModal';
+import AuthButtons from './AuthButtons';
 
 type Props = {};
 
 const RightContent = (props: Props) => {
-	return <div>RightContent</div>;
+	return (
+		<>
+			<AuthModal/>
+            <Flex justify="center" align="center">
+                <AuthButtons/>
+            </Flex>
+		</>
+	);
 };
 
 export default RightContent;
