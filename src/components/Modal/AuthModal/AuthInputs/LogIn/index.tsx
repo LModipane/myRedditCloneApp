1a127b64e725690dcde14b5d573176094e25970f
shaun <modipanesh@gmail.com>
2023-03-06 17:38:49 +0200

    added state to form and fixed styling

--- src/components/Modal/AuthModal/AuthInputs/LogIn/index.tsx
+++ src/components/Modal/AuthModal/AuthInputs/LogIn/index.tsx
@@ -1,14 +1,63 @@
 import { Button, Input } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
 
 type Props = {};
 
 const Login = (props: Props) => {
+	const [loginForm, setLoginForm] = useState({
+		email: '',
+		password: '',
+	});
+
+	const handelFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		//update state
+		setLoginForm(prev => ({
+			...prev,
+			[event.target.name]: event.target.value,
+		}));
+	};
+
 	return (
 		<form>
-			<Input name="email" type="email" placeholder="enter email" />
-			<Input name="password" type="password" placeholder="enter password" />
-			<Button type="submit">Log in</Button>
+			<Input
+				name="email"
+				type="email"
+				placeholder="enter email"
+				value={loginForm.email}
+				onChange={handelFormChange}
+				required
+				fontSize="10pt"
+				_hover={{ bg: 'gray.50', border: '1px solid', borderColor: 'blue.300' }}
+				_placeholder={{ color: 'gray.700' }}
+				_focus={{
+					outline: 'none',
+					border: '1px solid',
+					borderColor: 'blue.500',
+				}}
+				bg="gray.100"
+				mb="3"
+			/>
+			<Input
+				name="password"
+				type="password"
+				placeholder="enter password"
+				value={loginForm.password}
+				onChange={handelFormChange}
+				required
+				fontSize="10pt"
+				_hover={{ bg: 'gray.50', border: '1px solid', borderColor: 'blue.300' }}
+				_placeholder={{ color: 'gray.700' }}
+				_focus={{
+					outline: 'none',
+					border: '1px solid',
+					borderColor: 'blue.500',
+				}}
+				bg="gray.100"
+				mb="3"
+			/>
+			<Button width="100%" height="36px" mt="2" mb="2" type="submit">
+				Log in
+			</Button>
 		</form>
 	);
 };
