e96bd3efffd56cf6c721c89eb6366a880ebd727f
shaun <modipanesh@gmail.com>
2023-03-08 14:46:27 +0200

    create user doc when using OAuth sign in

--- src/components/Modal/AuthModal/OAuthButtons/index.tsx
+++ src/components/Modal/AuthModal/OAuthButtons/index.tsx
@@ -1,19 +1,33 @@
 import { Button, Flex, Image } from '@chakra-ui/react';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
-import { auth } from '@/firebase/clientApp';
-
+import { auth, firestore } from '@/firebase/clientApp';
+import { User } from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
 
 type Props = {};
 
 const OAuthButtons = (props: Props) => {
+	const [signInWithGoogle, userCred, loading, error] =
+		useSignInWithGoogle(auth);
+	const createUserDocument = async (user: User) => {
+		const userDocRef = doc(firestore, 'users', user.uid);
+		await setDoc(userDocRef, JSON.parse(JSON.stringify(user)));;
+	};
 
-	const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth)
+	useEffect(() => {
+		if (userCred) createUserDocument(userCred.user);
+	}, [userCred]);
 	return (
 		<Flex direction="column" width="100%" mb="4">
-			<Button variant="oauth" height="45px" fontSize="16" isLoading={loading} onClick={ ()=> signInWithGoogle()}>
-                <Image src="/google.svg" alt="google-logo" height="25px" mr="3 "/>
-                Continue with Google
+			<Button
+				variant="oauth"
+				height="45px"
+				fontSize="16"
+				isLoading={loading}
+				onClick={() => signInWithGoogle()}>
+				<Image src="/google.svg" alt="google-logo" height="25px" mr="3 " />
+				Continue with Google
 			</Button>
 		</Flex>
 	);
